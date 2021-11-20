bef3cae2e3e7723f856550d16cf61e344acc1100
KDubicki <kamil3234@gmail.com>
2021-11-20 17:57:17 +0100

    Update for keyboard buttons (animation)

--- js/modules/keyboard.js
+++ js/modules/keyboard.js
@@ -2,54 +2,59 @@
 const keyboard = document.querySelector('.game__keyboard')
 let keyboardActive = false
 const key = []
+const colors = []
+colors['dark'] = '#1B1A17'
+colors['orange'] = '#ff7900'
+colors['darkOrange'] = '#ff6d00'
 
 //Functions
 export const on = () => keyboardActive = true
 export const off = () => keyboardActive = false
 
-const changeButtonStyle = (button) => {
-    const changeTime = 600
-    let styles = [{
-        color: '#1B1A17',
-        background: '#ff7900',
-        borderColor: '#ff6d00',
-        boxShadow: `inset 2rem 2rem 3rem #ff6d00`
+const changeButtonColor = (button, color, time) => {
+    let keyframe = [{
+        color: color[0],
+        background: color[1],
+        boxShadow: `inset 2rem 2rem 3rem ${ color[2] }`
     }]
 
-    button.animate(styles, {
-        duration: changeTime,
+    button.animate(keyframe, {
+        duration: time,
     })
-    setTimeout(() => {
-        button.style.color = '#1B1A17'
-        button.style.background = '#ff7900'
-        button.style.borderColor = '#ff6d00'
-        button.style.boxShadow = `inset 2rem 2rem 3rem #ff6d00`
-    }, changeTime - 50)
 }
 
-const animateButtonDown = code => {
-    const querySelector = '[code="' + code + '"]'
-    const button = keyboard.querySelector(querySelector)
-    changeButtonStyle(button, 'red', 'green')
+const animateButtonDown = button => {
+    const changeTime = 300
+    const color = [colors['dark'], colors['orange'], colors['darkOrange']]
+    changeButtonColor(button, color, changeTime)
+    setTimeout(() => {
+        button.classList.add("game__button-active")
+    }, changeTime)
+}
+
+const animateButtonUp = button => {
+    const changeTime = 300
+    const color = [colors['orange'], colors['dark'], colors['dark']]
+    changeButtonColor(button, color, changeTime)
+    setTimeout(() => {
+        button.classList.remove("game__button-active")
+    }, changeTime)
 }
 
 export const keyDown = code => {
     if (!(keyboardActive && key[code] !== true)) return false
-    console.log(code)
-    key[code] = true
-    animateButtonDown(code)
-    return true
-}
 
-const animateButtonUp = () => {
+    key[code] = true
+    const button = keyboard.querySelector('[code="' + code + '"]')
+    animateButtonDown(button)
+    return true
 }
 
 export const keyUp = code => {
     if (!(keyboardActive && key[code] === true)) return;
 
     key[code] = false
-    animateButtonUp()
+    const button = keyboard.querySelector('[code="' + code + '"]')
+    animateButtonUp(button)
+    button.classList.remove("game__button-active")
 }
-
-
-
