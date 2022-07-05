b6bb04ddd0a3ad2b7e3a63559d1155707b777dbf
KDubicki <kamil3234@gmail.com>
2022-07-05 21:49:27 +0200

    Change some style to look better.

--- js/modules/result.js
+++ js/modules/result.js
@@ -9,12 +9,12 @@ export const show = ({ total, correct, time } = {}) => {
 
     const incorrect = total - correct;
     results.innerHTML = `
-          <p class="modal__result">Speed: ${ Math.round((total / time) * 100) / 100 } chars / s</p>
+          <p class="modal__result">Speed: ${ Math.round((total / time) * 100) / 100 } characters/s</p>
           <p class="modal__result">Accuracy: ${ Math.round((correct / total) * 100)}%</p>
           <p class="modal__result">Correct chars: ${ correct }</p>
-          <p class="modal__result">Incorrect chars: ${ incorrect }</p>
-          <p class="modal__result">Total chars: ${ total }</p>
-          <p class="modal__result">Total Time: ${ time }s</p>
+          <p class="modal__result">Incorrect characters: ${ incorrect }</p>
+          <p class="modal__result">Total characters: ${ total }</p>
+          <p class="modal__result">Total Time: ${ time } s</p>
     `;
 
     modal.classList.remove('hidden');
